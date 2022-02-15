194b5d1d60dc69cc15cfe46a2bd05c18f61c37d0
Roshan Khatale <rvkhatale@gmail.com>
2022-02-15 14:30:00 +0530

    Add recommender logic

--- scripts/recommend.py
+++ scripts/recommend.py
@@ -2,9 +2,8 @@ import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-data = pd.read_csv("data/movies.csv")
-count = CountVectorizer()
-matrix = count.fit_transform(data["genres"])
-similarity = cosine_similarity(matrix)
-
-print("Similarity matrix calculated.")
+df = pd.read_csv("data/movies.csv")
+cv = CountVectorizer()
+mat = cv.fit_transform(df['genres'])
+sim = cosine_similarity(mat)
+print("Similarity matrix ready.")
